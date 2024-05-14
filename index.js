d77215ce080a759f86a17577fc129a434d4a86a4
Ivan Silva <silvaivan280@gmail.com>
2024-05-14 16:21:44 -0300

    agregue el puerto y los middlewares

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
 import express from "express";
+import cors from "cors";
+import * as dotenv from "dotenv";
+import morgan from "morgan";
 
-console.log('esto es una prueba del backend');
+dotenv.config();
+
+const app = express();
+
+app.set('PORT', process.env.PORT || 4000);
+
+app.listen(app.get('PORT'), ()=>{
+    console.log('estoy en el puerto '+ app.get('PORT'));
+});
+
+app.use(express.json());
+app.use(express.urlencoded({ extended:true }));
+app.use(cors());
+app.use(morgan('dev'));
