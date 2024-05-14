f1c35fc27d3b0b48df175d265c0350c1d7a04ee0
Ivan Silva <silvaivan280@gmail.com>
2024-05-14 19:09:57 -0300

    agregue los controladores

--- src/routes/productos.routes.js
+++ src/routes/productos.routes.js
@@ -1,12 +1,8 @@
 import { Router } from "express"
+import { consultaListaProductos } from "../controllers/productos.controllers";
 
 const router = Router();
 
-router.route('/productos').get()
-
-// app.get('/productos', (req, res)=>{
-
-// })
-
+router.route('/productos').get(consultaListaProductos)
 
 export default router;
