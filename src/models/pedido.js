25689fbceb6a9b858790809a9f07412f56e16938
Ivan Silva <silvaivan280@gmail.com>
2024-05-16 16:27:45 -0300

    agregue el modelo de pedidos

---
+++ src/models/pedido.js
@@ -0,0 +1,28 @@
+import mongoose, {Schema} from "mongoose";
+
+const pedidoSchema = Schema({
+    nombreUsuario: {
+        type: String,
+        required: true,
+    },
+    fecha: {
+        type: String,
+        required: true,
+    },
+    productos: {
+        type: Array,
+        required: true,
+    },
+    estado:{
+        type: String,
+        required: true,
+    },
+    total:{
+        type: Number,
+        required: true,
+    }
+});
+
+const Pedido = mongoose.model('pedidos', pedidoSchema);
+
+export default Pedido;
